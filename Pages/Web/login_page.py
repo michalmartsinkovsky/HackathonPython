76d3bce724ff4504b0644691d60f35101a6e3e24
michalmartsinkovsky <mmichal1610@gmail.com>
2021-12-26 11:03:29 +0200

    building infrastructure

---
+++ Pages/Web/login_page.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+    class click_me_page():
+        def __init__(self, driver):
+            self.driver = driver
+
+    def get_user_name(self):
+        return self.driver.find_element(By.XPATH, "//input[@id='username']")
+
+    def get_password1(self):
+        return self.driver.find_element(By.XPATH, "//input[@id='password']")
+
+    def get_signin_btn(self):
+        return self.driver.find_element(By.XPATH, "//button[@data-test='signin-submit']")
+
+
